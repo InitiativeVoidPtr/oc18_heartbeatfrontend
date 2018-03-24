5bc13a787f6146b3309f65722b3dad967de49b21
derpatrick <patrick.piatkowski@gmail.com>
2018-03-24 14:40:45 +0100

    fontawesome and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {CommonModule} from '@angular/common';
 import {EkgOverviewComponent} from './ekg-overview/ekg-overview.component';
+import { EkgTableComponent } from './ekg-table/ekg-table.component';
+import { EkgChartComponent } from './ekg-chart/ekg-chart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, EkgOverviewComponent
+    AppComponent, EkgOverviewComponent, EkgTableComponent, EkgChartComponent
   ],
   imports: [
     BrowserModule, CommonModule
