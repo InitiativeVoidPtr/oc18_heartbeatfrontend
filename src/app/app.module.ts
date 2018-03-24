1b2989ca4426cc6e03f0d8e063bff10b3384e9be
derpatrick <patrick.piatkowski@gmail.com>
2018-03-24 14:22:10 +0100

    base component with style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CommonModule} from '@angular/common';
+import {EkgOverviewComponent} from './ekg-overview/ekg-overview.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EkgOverviewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ekg-overview/ekg-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ekg-overview',
+  templateUrl: './ekg-overview.component.html',
+  styleUrls: ['./ekg-overview.component.css']
+})
+export class EkgOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
