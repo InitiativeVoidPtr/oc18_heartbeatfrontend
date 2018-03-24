9b0f67c59d89eed008e4e83e082bf542477cc86e
derpatrick <patrick.piatkowski@gmail.com>
2018-03-24 15:44:38 +0100

    chart and table component

---
+++ src/app/ekg-chart/ekg-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ekg-chart',
+  templateUrl: './ekg-chart.component.html',
+  styleUrls: ['./ekg-chart.component.css']
+})
+export class EkgChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/ekg-table/ekg-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ekg-table',
+  templateUrl: './ekg-table.component.html',
+  styleUrls: ['./ekg-table.component.css']
+})
+export class EkgTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
